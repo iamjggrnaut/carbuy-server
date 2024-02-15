7333f1bd42bb0aff1413fff679de2193c725a13e
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-15 13:26:31 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = new Router()
 const mailingController = require('../controllers/mailingController')
 
 router.use('/mail/request-call', mailingController.postNumber)
-router.use('/mail/request-info', mailingController.postInfo)
+router.use('/mail/request-price', mailingController.postInfo)
 
 
 module.exports = router
