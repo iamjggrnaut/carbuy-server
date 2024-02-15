b064d90238921113420659cf62b4f7f654cbf6d9
iamjggrnaut <rustam.tahamtan@gmail.com>
2024-02-15 12:03:56 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
-const mailingRoute = require('./mailingRoute')
+const mailingController = require('../controllers/mailingController')
 
-router.use('/mail', mailingRoute)
+router.use('/mail/request-call', mailingController.postNumber)
+router.use('/mail/request-info', mailingController.postInfo)
 
 
 module.exports = router
